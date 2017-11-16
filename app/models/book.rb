0d8d9bfcbe1a0056bf16705abfaa6b6bf59016cd
DeadKennedyx <juan.dw.ft@gmail.com>
2017-11-16 17:07:23 -0600

    feature/add-model-validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+  has_many :user_books
+  has_many :users, through: :user_books
   has_many :book_categories
   has_many :categories, through: :book_categories
+
+  validates_format_of :name, :author, with: /\A[a-z]+\z/i
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many :books
+  has_many :user_books
+  has_many :books, through: :user_books
+
+  validates :email, email_format: { message: "doesn't look like an email address" }
+  validates_format_of :name, with: /\A[a-z]+\z/i
 end
